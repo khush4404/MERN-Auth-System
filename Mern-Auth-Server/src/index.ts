453f91a7597546449db3fd6c4fad7c8e305e0847
Khush-dbc <khush.dbc@gmail.com>
2025-10-03 10:50:48 +0530

    general changes

--- Mern-Auth-Server/src/index.ts
+++ Mern-Auth-Server/src/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.set("trust proxy", true);
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
